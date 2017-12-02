1b8e8e21061a8238f699fc4b01d1ac2b65b8d096
Mahmoud Zougheb <m.zougheb@gmail.com>
2017-12-02 04:19:11 -0800

    initial version

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-var express          = require("express"),
-    app              = express(),
-    expressSanitizer = require("express-sanitizer"),
-    methodOverride   = require("method-override"),
-    passport         = require("passport"),
-    LocalStrategy    = require("passport-local"),
-    User             = require("./models/user"),
-    bodyParser       = require("body-parser"),
-    mongoose         = require("mongoose"),
-    PORT             = process.env.PORT || 8081;
-
-
-// APP CONFIG
-mongoose.connect("mongodb://localhost/food_world_app",{useMongoClient: true});
-app.use(express.static("public"));
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    flash          = require("connect-flash"),
+    passport       = require("passport"),
+    LocalStrategy  = require("passport-local"),
+    methodOverride = require("method-override"),
+    Cuisine     = require ("./models/cuisine"),
+    Comment        = require ("./models/comment"),
+    User           = require("./models/user"),
+    PORT           = process.env.PORT || 8081,
+    seedDB         = require ("./seeds");
+ 
+// requiring routes
+var commentRoutes    = require("./routes/comments"),
+    cuisineRoutes = require("./routes/cuisines"),
+    indexRoutes      = require("./routes/index");
+    
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/food_world_app", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressSanitizer());
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); // seed the database
 
-// MONGOOSE/MODEL CONFIG
-var mealSchema = new mongoose.Schema({
-	title: String,
-	image: String,
-	body: String,
-	created: {type: Date, default: Date.now}
-});
-
-var Meal = mongoose.model("Meal", mealSchema);
+// Use moment.js
+app.locals.moment = require('moment');
 
 // PASSPORT CONFIG
 app.use(require("express-session")({
@@ -42,141 +43,15 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
    next();
 });
 
+app.use(indexRoutes);
+app.use(cuisineRoutes);
+app.use(commentRoutes);
 
-app.get("/", function(req, res){
-  res.render("landing")
-})
-
-
-// INDEX ROUTE
-app.get("/meals", function(req, res){
-  // RETRIEVE ALL THE MEALS FROM THE DB
-  Meal.find({}, function(err, meals){
-    if(err){
-      console.log("ERROR");
-    } else{
-      res.render("index", {meals:meals, currentUser: req.user});
-    }
-  }); 
-});
-
-// NEW ROUTE
-app.get("/meals/new", function(req,res){
-  res.render("new");
-});
-
-// CREATE ROUTE
-app.post("/meals", function(req, res){
-  // create meal
-  // req.body.meal.body = req.sanitizer(req.body.meal.body);
-  Meal.create(req.body.meal, function(err, newMeal){
-    if(err){
-      res.render("new");
-    } else{
-       // then, redirect to the index
-      res.redirect("/meals");
-    }
-  });
-});
-// SHOW ROUTE
-app.get("/meals/:id", function(req, res){
-  Meal.findById(req.params.id, function(err, foundMeal){
-    if(err){
-      res.redirect("/meals");
-    } else {
-      res.render("show", {meal: foundMeal});
-    }
-  });
-});
-
-// EDIT ROUTE
-app.get("/meals/:id/edit", function(req, res){
-   Meal.findById(req.params.id, function(err, foundMeal){
-       if(err){
-           console.log(err);
-           res.redirect("/meals");
-       } else {
-           res.render("edit", {meal: foundMeal});
-       }
-   });
-});
-
-// UPDATE ROUTE
-app.put("/meals/:id", function(req, res){
-  req.body.meal.body = req.sanitize(req.body.meal.body);
-  Meal.findByIdAndUpdate(req.params.id, req.body.meal, function(err, updtedMeal){
-       if(err){
-           res.redirect("/meals");
-       } else {
-         res.redirect("/meals/" + req.params.id);
-         
-       }
-   });
-});
-
-// DELETE ROUTE
-app.delete("/meals/:id", function(req, res){
-  // DESTROY MEAL
-   Meal.findByIdAndRemove(req.params.id, function(err, Meal){
-       if(err){
-           res.redirect("/meals");
-       } else {
-           res.redirect("/meals");
-       }
-   }); 
-});
-
-// ================
-// AUTH ROUTES
-// ================
-
-// SHOW REGISTER FORM
-app.get("/register", function(req, res){
-    res.render("signup");
-});
-// Handle Sign Up Logic
-app.post("/register", function(req, res){
-    // res.send("Signing you up!!");
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password,function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("signup");
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/meals");
-        });
-    });
-});
-
-// SHOW LOGIN FORM
-app.get("/login", function(req, res){
-    res.render("login");
-});
-// handling login logic
-app.post("/login", passport.authenticate("local",
-     {
-         successRedirect: "/meals",
-         failureRedirect: "/login"
-}), function(req, res){
-});
-
-// LOGOUT ROUTES AND LOGIC
-app.get("/logout", function(req, res){
-    req.logout();
-    res.render("landing");
-});
-
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function(){
+    console.log("APP Listening on PORT: " + PORT);
 });
