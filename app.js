5ec482265d4b5bd4cf73e78f51eccd7cdb801eb1
zougheb <m.zougheb@gmail.com>
2017-12-06 18:58:45 -0800

    editing the show page

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var express        = require("express"),
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
     MONGODB_URI    = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines",
-    options = {
-                  useMongoClient: true,
-                  autoIndex: false, // Don't build indexes
-                  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-                  reconnectInterval: 500, // Reconnect every 500ms
-                  poolSize: 10, // Maintain up to 10 socket connections
-                  // If not connected, return errors immediately rather than waiting for reconnect
-                  bufferMaxEntries: 0
-                },
+    options        = {
+                        useMongoClient: true,
+                        autoIndex: false, // Don't build indexes
+                        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+                        reconnectInterval: 500, // Reconnect every 500ms
+                        poolSize: 10, // Maintain up to 10 socket connections
+                        // If not connected, return errors immediately rather than waiting for reconnect
+                        bufferMaxEntries: 0
+                      },
     flash          = require("connect-flash"),
     passport       = require("passport"),
     LocalStrategy  = require("passport-local"),
